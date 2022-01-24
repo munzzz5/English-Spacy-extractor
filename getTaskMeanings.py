5ba9239510a0e521ba9ae8123e0afe24ca1834b1
Akshay Bhandari <akshay.b1@outlook.com>
2022-01-24 17:30:23 +0530

    refactoring, added ability to get left and right

--- getTaskMeanings.py
+++ getTaskMeanings.py
@@ -2,54 +2,69 @@ import pandas as pd
 import spacy
 
 
+nlp = spacy.load("en_core_web_md")
 
-nlp=spacy.load("en_core_web_md")
 
-def getWordData(doc,inword):
-    
-    dictWord=[{'word':word.text,'POS':word.pos_,'DEP':word.dep_,'lemma':word.lemma_} for word in doc if word.text==inword]
-    return dictWord[0]  
+def getWordData(doc, inword):
 
-if __name__ == "__main__":
-    df=pd.read_excel(input("Enter the file name with extension: "))
-    tasks=df[input('Enter the column name: ')]
+    dictWord = [{'word': word.text, 'POS': word.pos_, 'DEP': word.dep_, 'left': [tok.text for tok in word.lefts], 'right': [
+        tok.text for tok in word.rights], 'lemma': word.lemma_} for word in doc if word.text == inword]
+    return dictWord[0]
 
-    outDf={'Task':[],'word':[],'POS':[],'DEP':[],'lemma':[]}
+
+def getExcel(fileName):
+    df = pd.read_excel(fileName)
+    return df
+
+
+def saveAndExit(outDf):
+    print(outDf)
+    print('saving and exiting\n')
+    pd.DataFrame(outDf).to_excel(
+        input('Enter file to save')+".xlsx")
+    exit()
+
+
+def getColumnAndExtract():
+    tasks = df[input('Enter the column name: ')]
+
+    outDf = {'Task': [], 'word': [], 'POS': [],
+             'DEP': [], 'lemma': [], 'left': [], 'right': []}
     for task in tasks:
         print(task)
-        doc=nlp(task)
+        doc = nlp(task)
         # print(doc['check'].lemma_)
         outDf['Task'].append(task)
-        wordList=[token.text for token in doc]
+        wordList = [token.text for token in doc]
         # 1. ask user for important words
         # 2. if word in doc, get meaning, add to list
         # 3. break with x
         # 4. return list
-        firstWord=True
+        firstWord = True
         while(True):
-            inputWord=input("Enter word: (Enter 1 for next task||Enter 2 to stop completely and save your work)")
-            
-            if inputWord=='1': #break keyword
+            inputWord = input(
+                "Enter word: (Enter 1 for next task||Enter 2 to stop completely and save your work)")
+
+            if inputWord == '1':  # break keyword
                 print('next task\n')
-                firstWord=False
+                firstWord = False
                 break
-            elif inputWord=='2':
-                print(outDf)
-                print('saving and exiting\n')
-                pd.DataFrame(outDf).to_excel(input('Enter file to save')+".xlsx")
-                exit()
+            elif inputWord == '2':  # exit keyword
+                saveAndExit(outDf)
             if inputWord in wordList:
-                dTemp=getWordData(doc,inputWord)
+                dTemp = getWordData(doc, inputWord)
                 if not firstWord:
                     outDf['Task'].append('')
                 else:
-                    firstWord=False
+                    firstWord = False
                 for key in dTemp:
                     outDf[key].append(dTemp[key])
-                
+
             else:
                 print("Word isnt there! Try again\n\n")
                 continue
-            
-    
-          
+
+
+if __name__ == "__main__":
+    df = getExcel(input("Enter the file name: "))
+    getColumnAndExtract()
